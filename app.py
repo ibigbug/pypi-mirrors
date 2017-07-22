b7dbc5c132d6fd73841170bc296c95433a7b35ff
ibigbug <akabyw@gmail.com>
2017-07-23 05:26:00 +0800

    Add metrics

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
+import time
+
+from flask import g, request
 from flask import Flask, render_template, Response
 from utils import get_page_data, get_json_data
+
+from prometheus_client import make_wsgi_app
+from services.metrics import req_counter, rt_histogram
 
 app = Flask(__name__)
 
@@ -13,6 +19,34 @@ def index():
 @app.route('/data.json')
 def json_data():
     return Response(get_json_data(), mimetype='application/json')
+
+
+@app.route('/metrics')
+def metrics():
+    return make_wsgi_app()
+
+
+@app.route('/status')
+def healthcheck():
+    return 'ok'
+
+
+@app.before_request
+def before_request():
+    g.start_time = time.time()
+
+
+@app.after_request
+def after_request(res):
+    meth = request.method
+    endpoint = request.path
+    status = res.status_code
+    req_counter.labels(meth, endpoint, status).inc()
+
+    delta = time.time() - g.start_time
+    rt_histogram.labels(meth, endpoint, status).observe(delta)
+
+    return res
 
 
 if __name__ == '__main__':
